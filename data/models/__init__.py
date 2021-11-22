78ea4b78735593ce6fe3c993170569b8a92abfe1
Anna <annatruong96@gmail.com>
2021-11-22 12:52:07 +0100

    added customer class

--- data/models/__init__.py
+++ data/models/__init__.py
@@ -1,6 +1,25 @@
-def main():
-    pass
+from sqlalchemy import Integer, Column, String, ForeignKey
+
+from data.db import Base
 
 
-if __name__ == '__main__':
-    main()
+class Customer(Base):
+    __tablename__ = "customers"
+
+    id_customers = Column(Integer, primary_key=True, autoincrement=True)
+    first_name = Column(String(100), nullable=False)
+    last_name = Column(String(100), nullable=False)
+    company_name = Column(String(100))
+    phone = Column(String(45), nullable=False)
+    address1 = Column(String(150), nullable=False)
+    address2 = Column(String(150))
+    city = Column(String(100), nullable=False)
+    zip_code = Column(String(45), nullable=False)
+    country = Column(String(45), nullable=False)
+    sales_representant = Column(String(150), nullable=False)
+    states = Column(String(100))
+    customer_cars_reg_no = Column(String(20), ForeignKey('customer_cars.reg_no'))
+    orders_order_no = Column(Integer, ForeignKey('orders.order_no'))
+
+
+
