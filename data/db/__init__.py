cf95227ddf05f3d002aad043e82953b125432153
lennyrydweissner <rydweissner@gmail.com>
2021-11-22 11:27:51 +0100

    added connection string

--- data/db/__init__.py
+++ data/db/__init__.py
@@ -1,1 +1,22 @@
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
+from .db_config import *
+
+
+engine = sqlalchemy.create_engine(
+    f"mysql+mysqlconnector://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+
+# När man skapar egna klasser använder man declarative_base
+Base = declarative_base()
+Session = sessionmaker()
+
+# Engine är den delen som gör att vi kan prata med databasen.
+Session.configure(bind=engine)
+session = Session()
+
+
+
+
 
