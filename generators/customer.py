6c2811017065ad5898627b03393d85bbbf07b85a
lennyrydweissner <rydweissner@gmail.com>
2021-11-25 09:51:48 +0100

    common commit, before we try to change tables, going to create a new branch from this.

--- generators/customer.py
+++ generators/customer.py
@@ -1,24 +1,24 @@
 # from controllers.customer_controller import create_customer
+from controllers.customer_controller import create_customer
 
 
 def main():
-    pass
-    #  customer = {
-    #     'first_name': 'Kalle',
-    #     'last_name': 'Andersson',
-    #     'company_name': '',
-    #     'phone': '+46731246409',
-    #     'address1': 'Dragsspelsgatan 10A',
-    #     'address2': '',
-    #     'city': 'Göteborg',
-    #     'zip_code': '44873',
-    #     'country': 'Sweden',
-    #     'sales_representant': 'Gösta Gustavsson',
-    #     'states': '',
-    #     # 'orders_order_no': ''
-    # }
-    #
-    # create_customer(customer)
+    customer = {
+        'first_name': 'Kalle',
+        'last_name': 'Andersson',
+        'company_name': '',
+        'phone': '+46731246409',
+        'adress1': 'Dragsspelsgatan 10A',
+        'adress2': '',
+        'city': 'Göteborg',
+        'zip_code': '44873',
+        'country': 'Sweden',
+        'sales_representant': 'Gösta Gustavsson',
+        'states': '',
+        # 'orders_order_no': '123'
+    }
+
+    create_customer(customer)
 
 
 if __name__ == '__main__':
