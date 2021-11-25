064cffb27ac90ebdd957f54fc204282b66efe13e
lennyrydweissner <rydweissner@gmail.com>
2021-11-25 10:11:26 +0100

    added latest version of customer_repositroy.py

--- data/repository/customer_repositroy.py
+++ data/repository/customer_repositroy.py
@@ -2,12 +2,11 @@ from data.db import session
 from data.models import Customer
 
 
-# def get_customer_by_id(_id):
-#     return session.query(Customer).filter(Customer.id_customers == _id).first()
-#
-#
-# def create_customer(customer):
-#     customer = Customer(**customer)
-#     session.add(customer)
-#     session.commit()
+def get_customer_by_id(_id):
+    return session.query(Customer).filter(Customer.id_customers == _id).first()
 
+
+def create_customer(customer):
+    customer = Customer(**customer)
+    session.add(customer)
+    session.commit()
