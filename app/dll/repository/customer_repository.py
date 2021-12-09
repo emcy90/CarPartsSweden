c1a5f24417e673c48ce2d85b880769375dc76b7a
Emcy90 <emma@emcy.se>
2021-12-09 10:39:56 +0100

    Client not fully functional changes by Anna

--- app/dll/repository/customer_repository.py
+++ app/dll/repository/customer_repository.py
@@ -14,11 +14,19 @@ def get_customer_by_id():
         customer_id_clean_list.append(id[0])
     return customer_id_clean_list
 
-    # WITH COMPREHENSION
-    # customer_id = session.query(Customer.id_customers).all()
-    # customer_id_clean_list = [id[0] for id in customer_id]
-    # return customer_id_clean_list
 
+def show_all_customers():
+    show_customers = session.query(Customer.id_customers).all()
+
+
+
+
+    print(f'Customer id: {show_customers.id_customers}, First name: {show_customers.first_name},'
+            f' Last name: {show_customers.last_name}, Company name: {show_customers.company_name},'
+            f' Phone: {produkten.product_id}, Adress 2: {produkten.product_description},'
+            f' City: {show_customers.last_name}, Zipcode: {show_customers.company_name}',
+
+          )
 
 def create_customer(customer):
     customer = Customer(**customer)
